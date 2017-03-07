0d73bae02967570b69da2d5ba946c9879c18e755
Pratik Ghodekar <pratikg@citiustech.com>
2017-03-07 17:17:32 +0530

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# citius_testslave
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: citius_testslave
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
