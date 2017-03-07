3d96ec317f257e19267ddfc5fe43336b80c69d56
Pratik Ghodekar <pratikg@citiustech.com>
2017-03-07 17:56:11 +0530

    commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,15 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+include_recipe 'java'
+include_recipe 'git'
+include_recipe 'citius_jenkins::install_slave'
+
+tag('slave')
+ruby_block 'set `slave_labels` node attributes to test-server' do
+  block do
+    node.normal['slave_labels'] = 'test-server'
+    node.save
+  end
+end
