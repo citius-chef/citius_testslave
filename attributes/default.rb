973781d6af59a9d3c7b420bc5020596f3e8818d1
Pratik Ghodekar <pratikg@citiustech.com>
2017-03-09 13:27:31 +0530

    commit

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,2 @@
-default['java']['windows']['url'] == 'https://s3-us-west-2.amazonaws.com/pratikgstorage/jdk-8u121-windows-x64.exe'
+default['java']['jdk_version'] 	  = '8'
+default['java']['windows']['url'] = 'https://s3-us-west-2.amazonaws.com/pratikgstorage/jdk-8u121-windows-x64.exe'
