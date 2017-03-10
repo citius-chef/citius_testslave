7962672277909ec2a69796b77916a962497a23ea
Pratik Ghodekar <pratikg@citiustech.com>
2017-03-10 12:01:41 +0530

    commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -14,6 +14,17 @@ windows_path 'C:\Program Files\Java\jdk1.8.0_121\bin' do
   action :add
 end
 
+env 'JAVA_HOME' do
+  value 'C:\\Program Files\\Java\\jdk1.8.0_121'
+  notifies :restart, 'reboot[now]', :delayed
+end
+
+reboot 'now' do
+  action :nothing
+  reason 'needs reboot after setting JAVA_HOME.'
+  delay_mins 1
+end
+
 include_recipe 'git'
 include_recipe 'chrome'
 include_recipe 'firefox'
